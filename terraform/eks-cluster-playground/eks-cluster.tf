fba0ed90e6843bbb6e1f6acd347bf9463a439044
Javier Salinas <jsalinaspolo@gmail.com>
2021-04-21 15:22:42 +0200

    Create an admin role and attach to aws-auth

--- terraform/eks-cluster-playground/eks-cluster.tf
+++ terraform/eks-cluster-playground/eks-cluster.tf
@@ -1,3 +1,27 @@
+data "aws_iam_policy_document" "admin_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [module.eks.cluster_iam_role_arn]
+    }
+  }
+}
+
+resource "aws_iam_role" "admin" {
+  assume_role_policy = data.aws_iam_policy_document.admin_policy.json
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
@@ -10,9 +34,18 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
+  map_roles = [
+    {
+      rolearn = aws_iam_role.admin.arn
+      username = "user-admin"
+      groups   = ["system:masters"]
+    }
+  ]
+
   workers_group_defaults = {
     root_volume_type = "gp2"
   }
+
 
   worker_groups = [
     {
